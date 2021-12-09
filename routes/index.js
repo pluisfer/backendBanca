1561fe635ebc2e6a1157bd9da913409f41e311f5
Luis F Pacheco <pluisfer@gmail.com>
2021-12-08 20:45:08 -0500

    firt commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+const users = [
+  {
+    nombre: "Luis",
+    password: "password",
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.send("welcome to backend server");
+});
+
+router.post("/login", function (req, res) {
+  let result = users.find((user) => user.nombre == req.body.nombre);
+  if (result) {
+    if (result.password == req.body.password) {
+      res.status(200).send({
+        message: "Successful Login!!",
+      });
+    } else {
+      res.status(200).send({
+        message: "Password incorrect!",
+      });
+    }
+  } else {
+    res.status(200).send({
+      message: "User not found!!",
+    });
+  }
+});
+
+module.exports = router;
