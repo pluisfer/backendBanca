c96775ea1435be03ba38d99e37c0e43d33275bc9
Luis F Pacheco <pluisfer@gmail.com>
2021-12-15 14:26:43 -0500

    se mejora taskcontrollers.js en su modulo de exportar

--- controllers/task.controllers.js
+++ controllers/task.controllers.js
@@ -1,42 +1,39 @@
-const express = require("express");
-const router = express.Router();
-
 const users = [
   {
     nombre: "Luis",
     password: "password",
   },
 ];
+module.exports = {
+  welcome: function (req, res, next) {
+    res.send("welcome to backend server");
+  },
 
-const welcome = (req, res, next) => {
-  res.send("welcome to backend server");
-};
-
-/**
+  /**
 API Rest Login
 Descripción: Verifica usuario y contraseña con respecto al JSON.
 Ruta: /Login
 Entrada: usuario, password
 Método: POST
 Entrega: Status/message */
-const login = (req, res) => {
-  let result = users.find((user) => user.nombre == req.body.nombre);
-  if (result) {
-    if (result.password == req.body.password) {
-      res.status(200).send({
-        message: "Successful Login!!",
-        redirectUrl: "/Clientes",
-      });
+
+  login: function (req, res) {
+    let result = users.find((user) => user.nombre == req.body.nombre);
+    if (result) {
+      if (result.password == req.body.password) {
+        res.status(200).send({
+          message: "Successful Login!!",
+          redirectUrl: "/Clientes",
+        });
+      } else {
+        res.status(200).send({
+          message: "Password incorrect!",
+        });
+      }
     } else {
       res.status(200).send({
-        message: "Password incorrect!",
+        message: "User not found!!",
       });
     }
-  } else {
-    res.status(200).send({
-      message: "User not found!!",
-    });
-  }
+  },
 };
-
-exports = { welcome, login };
